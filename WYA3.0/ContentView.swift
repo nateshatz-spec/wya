96a1b66f514616e6f3ea9c8ba78cc5faee96cd88
nshatz7 <nateshatz08@icloud.com>
2026-04-18 14:55:33 -0400

    Initial Commit

---
+++ WYA3.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WYA3.0
+//
+//  Created by Nathaniel on 4/18/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WYA3.0/WYA3_0App.swift
@@ -0,0 +1,17 @@
+//
+//  WYA3_0App.swift
+//  WYA3.0
+//
+//  Created by Nathaniel on 4/18/26.
+//
+
+import SwiftUI
+
+@main
+struct WYA3_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
